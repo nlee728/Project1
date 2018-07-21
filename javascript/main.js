bfbd84565cf0c667129b6cb7dfc1713ec2c222b9
unknown <sundaresh.sankaran@gmail.com>
2018-07-21 13:27:01 -0400

    sdjh

--- javascript/main.js
+++ javascript/main.js
@@ -11,11 +11,20 @@ $('#submit-btn').on("click",function(event){
     // GRoup should be captured
         var group = $("#group-input").val();
 
-        // Check the zipcode
-        zipValid(zipcode);
+        var zc=false;
 
-    //Check if Group already exists
-        groupCheck(group);
+        do while(zc=false) {
+                    // Check the zipcode
+                    zipValid(zc,zipcode);
+           
+        } ;
+
+        //Check if Group already exists
+         groupCheck(group);
+
+
+
+    
 
 
 
@@ -23,40 +32,47 @@ $('#submit-btn').on("click",function(event){
 
 // Functions "hoisted" here
 
-function zipValid(zipcode){
+function zipValid(zc,zipcode){
 
     regexZip=new RegExp('/^[0-9]{5}$/');
+
+    zipcode = zipcode.replace(/\D/,'');
 
     if (zipcode === "") {
 
         alert("Enter a zip");
+        return zc;
     
     } else if (regexZip.test(zipcode)==true) {
 
         alert("Do not enter any non number characters");
+        return zc=false;
 
     } else if (zipcode.length < 5) {
 
         alert("Zip should be 5 digits (4 digit ZIP4s are not needed ) ");
 
-    } else return;
+    } else return zc=true;
 };
 
 function groupCheck(group){
     var groups=['Hyatt House Hyenas','Morrisville Morons','Raleigh Rascals','Macaroni Cheese Pizza'];
+
 
     if (groups.indexOf(group)==true) {
         return;
     } else {
         $(".card-body").append("Group does not exist - Click add to create the same.",{id:"add-note"})
                         .append($("<p>"))
-                        .append($("<button>",{text:"Add group",class:"btn-primary",id:"add-group"}));
+                        .append($("<button>",{text:"Add group",class:"btn-primary",id:"add-group"}))
+                        .append($("<button>",{text:"Forget it",class:"btn-danger",id:"no-group"}));
+        
         
         $("#add-group").on("click",function(event){
             groups.push(group);
-            $("#add-note").empty();
+            $(this).text="";
             console.log(groups);
-        });
+           });
 
     } ;
 };
