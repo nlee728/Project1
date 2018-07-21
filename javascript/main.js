c979c83cc0ae2fdc068e55f6dd07ee3cd88d7075
unknown <sundaresh.sankaran@gmail.com>
2018-07-21 12:56:47 -0400

    Changes made to add a group in case it does not exist - please review

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,63 @@
-
-
 console.log("in");
+$('#submit-btn').on("click",function(event){
 
-//Grab values from all elements
+        // Preventing the buttons default behavior when clicked (which is submitting a form)
+        event.preventDefault();
+    
+    
+    // Capture zipcode
+        var zipcode = $("#zipcode-input").val();
 
-var submit=$("#submit-btn").text();
+    // GRoup should be captured
+        var group = $("#group-input").val();
 
-var suggestions = $("#movie-table");
+        // Check the zipcode
+        zipValid(zipcode);
 
+    //Check if Group already exists
+        groupCheck(group);
 
-// console.log(submit);
-// console.log(suggestions);
-
-$("#submit-btn").on("click",function(event){
-
-    // Preventing the buttons default behavior when clicked (which is submitting a form)
-    event.preventDefault();
-
-        var zipcode = $("#zipcode-input").text();
-        console.log("You clicked "+zipcode);
-
-        if (zipcode===""){
-            alert("No Zipcode entered!");
-        } else if (zipcode.length<5){
-            alert("Zipcode not valid - please enter 5 digit zipcode and 4 digits Zip4 (optional)");
-        };
 
 
 });
+
+// Functions "hoisted" here
+
+function zipValid(zipcode){
+
+    regexZip=new RegExp('/^[0-9]{5}$/');
+
+    if (zipcode === "") {
+
+        alert("Enter a zip");
+    
+    } else if (regexZip.test(zipcode)==true) {
+
+        alert("Do not enter any non number characters");
+
+    } else if (zipcode.length < 5) {
+
+        alert("Zip should be 5 digits (4 digit ZIP4s are not needed ) ");
+
+    } else return;
+};
+
+function groupCheck(group){
+    var groups=['Hyatt House Hyenas','Morrisville Morons','Raleigh Rascals','Macaroni Cheese Pizza'];
+
+    if (groups.indexOf(group)==true) {
+        return;
+    } else {
+        $(".card-body").append("Group does not exist - Click add to create the same.",{id:"add-note"})
+                        .append($("<p>"))
+                        .append($("<button>",{text:"Add group",class:"btn-primary",id:"add-group"}));
+        
+        $("#add-group").on("click",function(event){
+            groups.push(group);
+            $("#add-note").empty();
+            console.log(groups);
+        });
+
+    } ;
+};
+
